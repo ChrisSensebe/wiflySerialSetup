fd278bb272f23d42ff96496acb4e0122da46a0e2
ChrisSensebe <christophe.sensebe@gmail.com>
2016-11-27 23:07:20 +0100

    adding read and write toserial port

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
 const COMMANDS =
 {
     LIST_PORTS: '--list',
-    READ_PORT: '--read'
+    READ_PORT: '--read',
+    WRITE_AND_LISTEN: '--write'
 };
 let command = process.argv[2];
 let portName = process.argv[3];
@@ -22,13 +23,36 @@ if(command === COMMANDS.LIST_PORTS)
     });
 }
 
-// Read from portName
-if(command == COMMANDS.READ_PORT)
+// read from serial port
+if(command === COMMANDS.READ_PORT)
 {
-    let port = new SerialPort(portName);
+    let port = new SerialPort(portName, {parser: SerialPort.parsers.readline('\r\n')});
+    port.on('data', (data) => {
+        console.log(data);
+    });
+    port.on('error', (err) =>
+    {
+        console.error(err);
+    });
+}
 
+// write to port and listen to response
+if(command === COMMANDS.WRITE_AND_LISTEN)
+{
+    let port = new SerialPort(portName, {parser: SerialPort.parsers.readline('\r\n')});
+    port.on('data', (data) =>
+    {
+        console.log(data);
+    });
+    port.on('error', (err) =>
+    {
+        console.error(err);
+    });
     port.on('open', () =>
     {
-        console.log(`${portName} open, rate: ${port.options.baudRate} bauds`);
+        port.write('$$$\r\nscan\r\n', (err) =>
+        {
+            console.error(err);
+        });
     });
 }
