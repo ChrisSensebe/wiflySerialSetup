73a9854ecde08e5e2d3598fd3d34d517db0b087f
ChrisSensebe <christophe.sensebe@gmail.com>
2017-01-02 09:28:03 +0100

    app.js refactoring

--- app.js
+++ app.js
@@ -1,61 +1,52 @@
 /**
  * Created by nizural on 27/11/16.
  */
-const OPTIONS = {
-    LIST_PORTS: '--list',
-    READ_PORT: '--read',
-    WRITE_AND_LISTEN: '--write'
-};
-
-const COMMANDS = ['$$$', '\r\nscan\r\n', '\r\nscan\r\n', '\r\nscan\r\n',];
-
-let option = process.argv[2];
-let portName = process.argv[3];
+// importing node-serialport (https://github.com/EmergingTechnologyAdvisors/node-serialport)
 let SerialPort = require('serialport');
+// commands written to port
+const COMMANDS = ['$$$\r\n', 'scan\r\n', 'scan\r\n',];
+// interval between writes in milliseconds
+const COMMANDS_INTERVAL = 5000;
 
-// list SerialPort ports
+// search device connected on usb
 if(option === OPTIONS.LIST_PORTS){
     SerialPort.list(function (err, ports) {
         ports.forEach((port) => {
             if(/USB/.test(port.comName)){
                 console.log(port.comName);
+                openPort(port.comName);
             }
         });
     });
 }
 
-// read from serial port
-if(option === OPTIONS.READ_PORT) {
+/**
+ * write an d listen to serial port
+ * @param portName : name of the port
+ */
+function openPort(portName) {
+    // create port
     let port = new SerialPort(portName, {parser: SerialPort.parsers.readline('\r\n')});
+    // log data received from port
     port.on('data', (data) => {
         console.log(data);
     });
+    // log errors from port
     port.on('error', (err) => {
         console.error(err);
     });
-}
-
-// write to port and listen to response
-if(option === OPTIONS.WRITE_AND_LISTEN) {
-    let port = new SerialPort(portName, {parser: SerialPort.parsers.readline('\r\n')});
-    port.on('data', (data) => {
-        console.log(data);
-    });
-    port.on('error', (err) => {
-        console.error(err);
-    });
+    // on open, write commands to port
     port.on('open', () => {
-        console.log('port open');
-
+        console.log(`port ${portName} open`);
+        // position of current command in COMMANDS
         let currentCommand = 0;
-
+        // write a command to port every COMMANDS_INTERVAL milliseconds
         setInterval(() => {
             if(currentCommand < COMMANDS.length){
-                console.log('write command ', currentCommand);
                 port.write(COMMANDS[currentCommand++]);
             } else {
                 process.exit();
             }
-        }, 6000);
+        }, COMMANDS_INTERVAL);
     });
 }
